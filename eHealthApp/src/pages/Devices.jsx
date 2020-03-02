323ed15185cfa8a11016dd664acc608973956d41
Irek <b6020731@shu.ac.uk>
2020-03-02 23:33:05 +0000

    Started implementing tests for SensorSimulation task's components

--- eHealthApp/src/pages/Devices.jsx
+++ eHealthApp/src/pages/Devices.jsx
@@ -8,12 +8,25 @@ Author: Ireneusz Janusz
 
 // External dependencies
 import React, { useState, useEffect } from "react";
-import { IonContent, IonPage } from "@ionic/react";
+import { IonContent, IonPage, IonLabel, IonItem } from "@ionic/react";
 import BackButtonToolbar from "../components/BackButtonToolbar";
 import DeviceCard from "../components/DeviceCard";
 
 // Internal dependencies
 import BluetoothSynchronisationManager from '../bluetooth/managers/BluetoothSynchronisationManager';
+
+// Styling
+const styles = {
+	labelContainer: {
+		height: '100%',
+		display: 'flex'
+	},
+	noDevicesLabel: {
+		textAlign: 'center',
+		justifyContent: 'center',
+		fontSize: "30px"
+	}
+}
 
 /*
  *props:
@@ -42,7 +55,6 @@ const Devices = props => {
 
 	const deviceClickHandler = id => {
 		const connectingDevice = pairedDevices.find(device => device.id === id);
-		console.log(connectingDevice);
 		if (connectingDevice.connected) {
 			setPairedDevices(() => {
 				return changeDeviceStatus(id, "DISCONNECT")
@@ -92,17 +104,19 @@ const Devices = props => {
 		<IonPage>
 			<BackButtonToolbar title="Devices" />
 			<IonContent className="ion-padding">
-				{pairedDevices.map(x => {
-					return (
-						<DeviceCard
-							key={Math.random()}
-							title={x.name}
-							connected={x.connected}
-							connectionStatus={x.connectionStatus}
-							onClick={deviceClickHandler.bind(this, x.id)}
-						/>
-					)
-				})}
+				{
+					pairedDevices.length > 0 ? pairedDevices.map(x => {
+						return (
+							<DeviceCard
+								key={x.id}
+								title={x.name}
+								connected={x.connected}
+								connectionStatus={x.connectionStatus}
+								onClick={deviceClickHandler.bind(this, x.id)}
+							/>
+						)
+					}) : <IonItem lines="none" style={styles.labelContainer}><IonLabel style={styles.noDevicesLabel}>No devices found</IonLabel></IonItem>
+				}
 			</IonContent>
 		</IonPage>
 	);
