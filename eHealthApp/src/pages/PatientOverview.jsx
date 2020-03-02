c7fb9f6b705cb30383c3e20f6e6798bcf96a35c4
danielmadu <danielmadu@outlook.com>
2020-03-02 15:53:44 +0000

    Refactored render function to make it more automatic. Implemented the filter function

--- eHealthApp/src/pages/PatientOverview.jsx
+++ eHealthApp/src/pages/PatientOverview.jsx
@@ -4,72 +4,134 @@ Add description
 Author: Daniel Madu
 */
 
-import React from "react";
+import React, { useState } from "react";
 import { IonPage, 
 		IonContent, 
 		IonCard,
 		IonCardContent, 
 		IonList,
 		IonItem, 
-		IonCardHeader,
 		IonGrid,
-		IonCardTitle,
 		IonLabel,
+		IonIcon,
+		IonTitle,
+		useIonViewWillEnter,
+		IonRow,
+		IonCol,
 		IonSelect,
-		IonSelectOption} from "@ionic/react";
+		IonSelectOption,
+		IonToolbar} from "@ionic/react";
+import { heart } from 'ionicons/icons';
 import BackButtonToolbar from "../components/BackButtonToolbar";
+import { Container, Row, Col } from "react-bootstrap";
 
 /*props:
  */
+
 const PatientOverview = props => {
+	const [userData, setUserData] = useState([])
+	const todaysDate = new Date()
+	const yesterdaysDate = new Date(todaysDate)
+	const [selectedFilter, setSelectedFilter] = useState([])
+
+	const styles = {
+		activity: {
+			display: "flex"
+		},
+		icon: {
+			float: "left"
+		},
+		title: {
+			float: "center"
+		},
+		value: {
+			float: "right"
+		},
+		filter: {
+			width: "20%"
+		}
+	};
+
+	const RecordCard = props => {
+		return(
+			props.data
+			.filter(val => selectedFilter.includes(val.name) || selectedFilter.length === 0)
+			.map((d, i) =>
+				<IonCard key={i}>
+					<IonCardContent>
+						<IonGrid>
+							<IonRow style={styles.activity}>
+								<IonCol>
+									<IonLabel style={styles.icon}>
+										{d.name === "Blood pressure" && <IonIcon icon={heart} />}
+									</IonLabel>
+									<IonLabel style={styles.title}>
+										<h2>{d.name}</h2>
+									</IonLabel>
+								</IonCol>
+								<IonCol>									
+									<IonLabel>
+										<h3>{d.value}</h3>
+									</IonLabel>
+								</IonCol>
+							</IonRow>
+						</IonGrid>
+					</IonCardContent>
+				</IonCard>
+			)
+		)
+	}
+
+	useIonViewWillEnter(async () => {
+		yesterdaysDate.setDate(todaysDate.getDate() - 1)
+		setUserData([
+			{name:"Blood pressure", value:"120 mmHg", date: todaysDate.toDateString(), time:	"17:41"},
+			 {name:"Blood pressure", value:"130 mmHg", date: todaysDate.toDateString(), time:	"17:41"},
+			 {name:"Cycle", value:"20 mins", date: todaysDate.toDateString(), time:	"17:41"},
+			 {name:"Run", value:"25 mins", date: todaysDate.toDateString(), time:	"17:41"},
+		 	{name:"Blood pressure", value:"135 mmHg", date: yesterdaysDate.toDateString(), time: "05:43"},
+			{name:"Blood pressure", value:"140 mmHg", date: yesterdaysDate.toDateString(), time: "16:31"}
+		])
+	})
+
+	const setSelectedFilterHandler = (event) => {
+		setSelectedFilter(event.target.value)
+	}
+
     return(
         <IonPage>
             <BackButtonToolbar title={props.match.params.name + "'s " + "Overview"}/>
+			<IonToolbar>
+				<IonList>
+					<IonItem>
+						<IonLabel>Filter by</IonLabel> 
+						<IonSelect multiple={true} onIonChange={(event) => setSelectedFilterHandler(event)}>
+							<IonSelectOption>Blood pressure</IonSelectOption>
+							<IonSelectOption>Run</IonSelectOption>
+							<IonSelectOption>Walk</IonSelectOption>
+							<IonSelectOption>Cycle</IonSelectOption>
+							<IonSelectOption>Weight</IonSelectOption>
+						</IonSelect>
+					</IonItem>
+				</IonList>
+			</IonToolbar>
             <IonContent className="ion-padding">
 				<IonList>
 					<IonItem>
-						<IonLabel>Filter by</IonLabel>
-						<IonSelect multiple={true}>
-							<IonSelectOption>Blood pressure</IonSelectOption>
-							<IonSelectOption>Exercise</IonSelectOption>
-						</IonSelect>
-					</IonItem>
-					<IonItem>
 						<IonGrid>
-							<IonCardTitle>Today</IonCardTitle>
-							<IonCard>
-								<IonCardHeader>
-									<IonCardTitle>Blood pressure</IonCardTitle>
-								</IonCardHeader>
-								<IonCardContent>
-									<p>120 mmHg</p>
-									<p>Time taken: 17:41</p>
-								</IonCardContent>
-							</IonCard>
+							<IonTitle>Today</IonTitle>
+							{userData.map((data, i) => 
+								data.date == todaysDate.toDateString() && <RecordCard key={i} data={[data]}/>
+							)}
 						</IonGrid>
 					</IonItem>
 
 					<IonItem>
 						<IonGrid>
-							<IonCardTitle>Yesterday</IonCardTitle>
-							<IonCard>
-								<IonCardHeader>
-									<IonCardTitle>Blood pressure</IonCardTitle>
-								</IonCardHeader>
-								<IonCardContent>
-									<p>135 mmHg</p>
-									<p>Time taken: 05:43</p>
-								</IonCardContent>
-							</IonCard>
-							<IonCard>
-								<IonCardHeader>
-									<IonCardTitle>Blood pressure</IonCardTitle>
-								</IonCardHeader>
-								<IonCardContent>
-									<p>140 mmHg</p>
-									<p>Time taken: 16:31</p>
-								</IonCardContent>
-							</IonCard>
+							<IonTitle>Yesterday</IonTitle>
+							{userData.map((data, i) => 
+								data.date == yesterdaysDate.toDateString() && <RecordCard key={i} data={[data]}/>
+							)}
 						</IonGrid>
 					</IonItem>
 				</IonList>
--- eHealthApp/src/pages/Patients.jsx
+++ eHealthApp/src/pages/Patients.jsx
@@ -13,78 +13,108 @@ import { IonPage,
 		IonSelect, 
 		IonSelectOption, 
 		IonLabel, 
-		useIonViewWillEnter } from "@ionic/react";
+		useIonViewWillEnter, 
+		IonList,
+		IonItem,
+		IonToolbar} from "@ionic/react";
 import BackButtonToolbar from "../components/BackButtonToolbar";
 // import { IonActionSheet } from "@ionic/react";
 
 /*props:
  */
 const Patients = props => {
-	const [doctorData, setDoctorData] = useState([])
-	const [doctor, setDoctor] = useState("")
-	const [patientData, setPatientData] = useState([])
+	// const [doctorData, setDoctorData] = useState([])
+	const [doctor, setDoctor] = useState('')
+	const [dbData, setDBData] = useState([])
+	// const [patientData, setPatientData] = useState([])
 
 	const [showActionSheet, setShowActionSheet] = useState(false);
 
 
 	useIonViewWillEnter(() => {
-		setDoctorData([{id: 'dq24fd1', name: "Daniel"},
-						{id: 'eq24fd1', name: "Irek"},
-						{id: 'fq24fd1', name: "Andy"},
-						{id: 'gq24fd1', name: "Gergo"},
-						{id: 'hq24fd1', name: "Mark"},
-						{id: 'iq24fd1', name: "Sergio"},
-						{id: 'jq24fd1', name: "Margory"},
-						{id: 'kq24fd1', name: "Basu"},
-						{id: 'lq24fd1', name: "Chris"},
-						{id: 'mq24fd1', name: "Jing"},
-						{id: 'nq24fd1', name: "Gergo 2"},
-						{id: 'oq24fd1', name: "Simon"}])
+		// setDoctorData([{id: 'dq24fd1', name: "Daniel"},
+		// 				{id: 'eq24fd1', name: "Irek"},
+		// 				{id: 'fq24fd1', name: "Andy"},
+		// 				{id: 'gq24fd1', name: "Gergo"},
+		// 				{id: 'hq24fd1', name: "Mark"},
+		// 				{id: 'iq24fd1', name: "Sergio"},
+		// 				{id: 'jq24fd1', name: "Margory"},
+		// 				{id: 'kq24fd1', name: "Basu"},
+		// 				{id: 'lq24fd1', name: "Chris"},
+		// 				{id: 'mq24fd1', name: "Jing"},
+		// 				{id: 'nq24fd1', name: "Gergo 2"},
+		// 				{id: 'oq24fd1', name: "Simon"}])
 
-		setPatientData([{id: 'dq24fd1', name: "Daniel"},
-						{id: 'eq24fd1', name: "Irek"},
-						{id: 'fq24fd1', name: "Andy"},
-						{id: 'gq24fd1', name: "Gergo"},
-						{id: 'hq24fd1', name: "Mark"},
-						{id: 'iq24fd1', name: "Sergio"},
-						{id: 'jq24fd1', name: "Margory"},
-						{id: 'kq24fd1', name: "Basu"},
-						{id: 'lq24fd1', name: "Chris"},
-						{id: 'mq24fd1', name: "Jing"},
-						{id: 'nq24fd1', name: "Gergo 2"},
-						{id: 'oq24fd1', name: "Simon"}])
+		// setPatientData([{id: 'dq24fd1', name: "Daniel"},
+		// 				{id: 'eq24fd1', name: "Irek"},
+		// 				{id: 'fq24fd1', name: "Andy"},
+		// 				{id: 'gq24fd1', name: "Gergo"},
+		// 				{id: 'hq24fd1', name: "Mark"},
+		// 				{id: 'iq24fd1', name: "Sergio"},
+		// 				{id: 'jq24fd1', name: "Margory"},
+		// 				{id: 'kq24fd1', name: "Basu"},
+		// 				{id: 'lq24fd1', name: "Chris"},
+		// 				{id: 'mq24fd1', name: "Jing"},
+		// 				{id: 'nq24fd1', name: "Gergo 2"},
+		// 				{id: 'oq24fd1', name: "Simon"}])
+		
+		setDBData([
+					{name: 'Doctor Daniel', ID: 'Doctor-1234',
+					patients: [{id: 'Patient-1234', name: 'Daniel'},{id: 'Patient-1234', name: 'Irek'},{id: 'Patient-1234', name: 'Andy'},{id: 'Patient-1234', name: 'Greg'},
+							   {id: 'Patient-1234', name: 'Daniel2'},{id: 'Patient-1234', name: 'Irek2'},{id: 'Patient-1234', name: 'Andy2'},{id: 'Patient-1234', name: 'Greg2'}]},
+				
+					{name: 'Doctor Mark', ID: 'Doctor-5678',
+					patients: [{id: 'Patient-5678', name: 'Sergio'},{id: 'Patient-5678', name: 'Chris'},{id: 'Patient-5678', name: 'Margory'},{id: 'Patient-5678', name: 'Basu'}]}
+				])
+		
 	})
 
-
-	const displayPatientData = (data) => {
-        // setShowPatientData(true)
-		// setUserId(data)
-		console.log(data)
-	}
-	
 	const selectDoctorHandler = (event) => {
 		setDoctor(event.target.value)
 	}
 
-	const selectDoctorHandler2 = (id) => {
-		console.log(id)
-		setDoctor(id)
+	const Patient = (data) => {
+		return(
+			<IonCard routerDirection="forward" routerLink={"/patientoverview/" + data.patient.name + "/" + data.patient.id}>
+				<IonCardHeader>
+					<IonCardTitle>Patient's Name: {data.patient.name}</IonCardTitle>
+				</IonCardHeader>
+				<IonCardContent>
+					<p>
+						Patient Id: {data.patient.id}
+					</p>
+				</IonCardContent>
+			</IonCard>
+		)
 	}
 
 	return (
 		<IonPage>
+			<IonToolbar className="ion-padding">
+					<IonLabel>Select doctor</IonLabel>
+					<IonSelect interface="popover" onIonChange={(event) => selectDoctorHandler(event)}>
+						{dbData.map((doctor, index) => 
+							<IonSelectOption value={doctor.ID} key={index}>
+								{doctor.name}
+							</IonSelectOption>
+						)}
+					</IonSelect>
+			</IonToolbar>
 			<BackButtonToolbar title={"Patients"} />
 			<IonContent className="ion-padding">		
 				{/* <h1>Select a health professional from the list below to view their patients and their data.</h1> */}
-				<h2>Doctor</h2>
-				<IonLabel>Select doctor</IonLabel>
-				<IonSelect onIonChange={(event) => selectDoctorHandler(event)}>
-					{doctorData.map((doctor, index) => 
-						<IonSelectOption value={doctor.id} key={index}>
-							{doctor.name}
-						</IonSelectOption>
-					)}
-				</IonSelect>
+				<IonList>
+					{/* <IonItem>
+						<IonLabel>Select doctor</IonLabel>
+						<IonSelect interface="popover" onIonChange={(event) => selectDoctorHandler(event)}>
+							{doctorData.map((doctor, index) => 
+								<IonSelectOption value={doctor.id} key={index}>
+									{doctor.name}
+								</IonSelectOption>
+							)}
+						</IonSelect>
+					</IonItem> */}
+				
 
 
 				{/* <IonButton onClick={() => setShowActionSheet(true)} expand="block">Select doctor</IonButton>
@@ -101,25 +131,17 @@ const Patients = props => {
 					}
 				></IonActionSheet> */}
 
-
-
 				{doctor && 
-				<Fragment>
-					<h2>Assigned Patients</h2>
-					{patientData.map((patient, i) => 
-					<IonCard key={i} routerDirection="forward" routerLink={"/patientoverview/" + patient.name + "/" + patient.id} onClick={() => displayPatientData(patient.id)}>
-						<IonCardHeader>
-							<IonCardTitle>Patient's Id: {patient.id}</IonCardTitle>
-						</IonCardHeader>
-						<IonCardContent>
-							<p>
-								Patient Name: {patient.name}
-							</p>
-						</IonCardContent>
-					</IonCard>
-				)}
-				</Fragment>
-				}
+					<div>
+						<h2>Assigned Patients</h2>
+						{dbData.filter((doc) => doc.ID === doctor)
+						.map((patients, i1) => 
+							patients.patients.map((patient, i2) => 
+								<Patient key={i1+i2} patient={patient} />
+							)
+						)}
+					</div>}
+				</IonList>
 			</IonContent>
 		</IonPage>
 	);
