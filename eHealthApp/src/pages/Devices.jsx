6e643304f66ae22d0620d6ebdef32e7d5df4202c
Irek <b6020731@my.shu.ac.uk>
2020-03-04 10:21:01 +0000

    Testing Git Hooks integration

--- eHealthApp/src/pages/Devices.jsx
+++ eHealthApp/src/pages/Devices.jsx
@@ -7,10 +7,10 @@ Author: Ireneusz Janusz
 */
 
 // External dependencies
-import React, { useState, useEffect } from "react";
-import { IonContent, IonPage, IonLabel, IonItem } from "@ionic/react";
-import BackButtonToolbar from "../components/BackButtonToolbar";
-import DeviceCard from "../components/DeviceCard";
+import React, { useState, useEffect } from 'react';
+import { IonContent, IonPage, IonLabel, IonItem } from '@ionic/react';
+import BackButtonToolbar from '../components/BackButtonToolbar';
+import DeviceCard from '../components/DeviceCard';
 
 // Internal dependencies
 import BluetoothSynchronisationManager from '../bluetooth/managers/BluetoothSynchronisationManager';
@@ -37,10 +37,10 @@ const Devices = () => {
   useEffect(() => {
     getPairedDevices();
 
-		const synchronisationListener = setInterval(() => {
-			// console.log("it lives");
-		}, 5000);
-	}, [getPairedDevices])
+    const synchronisationListener = setInterval(() => {
+      // console.log("it lives");
+    }, 5000);
+  }, [getPairedDevices]);
 
   /**
    * Gets the list of available Bluetooth devices using the Bluetooth Synchronisation Manager
@@ -102,30 +102,30 @@ const Devices = () => {
     }
   };
 
-	/**
-	 * Changes the device's connection status using React Hooks
-	 * Function has been encapsulated as it repeats quite often in this component
-	 * In order to successfuly rerender the component, 
-	 * the device we are updating has to be removed from the devices array,
-	 * added back again and then array has to be sorted alphabetically
-	 * @param {String} id - ID of device
-	 * @param {String} status - Connection status of the device 
-	 */
-	const changeDeviceStatus = (id, status) => {
-		const connectingDevice = pairedDevices.find(device => device.id === id);
-		if (status === "CONNECTING") {
-			connectingDevice.changeConnectionStatus(status);
-		} else if (status === "DISCONNECT") {
-			BluetoothSynchronisationManager.disconnect(id);
-			connectingDevice.disconnect();
-		} else if (status === "FAILED"){
-			connectingDevice.failedToConnect();
-		}
-		const filteredPairedDevices = pairedDevices.filter(device => device.id !== id);
-		filteredPairedDevices.push(connectingDevice);
-		filteredPairedDevices.sort((a, b) => a.name.localeCompare(b.name));
-		return filteredPairedDevices;
-	};
+  /**
+   * Changes the device's connection status using React Hooks
+   * Function has been encapsulated as it repeats quite often in this component
+   * In order to successfuly rerender the component,
+   * the device we are updating has to be removed from the devices array,
+   * added back again and then array has to be sorted alphabetically
+   * @param {String} id - ID of device
+   * @param {String} status - Connection status of the device
+   */
+  const changeDeviceStatus = (id, status) => {
+    const connectingDevice = pairedDevices.find(device => device.id === id);
+    if (status === 'CONNECTING') {
+      connectingDevice.changeConnectionStatus(status);
+    } else if (status === 'DISCONNECT') {
+      BluetoothSynchronisationManager.disconnect(id);
+      connectingDevice.disconnect();
+    } else if (status === 'FAILED') {
+      connectingDevice.failedToConnect();
+    }
+    const filteredPairedDevices = pairedDevices.filter(device => device.id !== id);
+    filteredPairedDevices.push(connectingDevice);
+    filteredPairedDevices.sort((a, b) => a.name.localeCompare(b.name));
+    return filteredPairedDevices;
+  };
 
   return (
     <IonPage>
