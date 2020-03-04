c76679e8b0d576ab1cbb1c92122c37979428f368
Irek <b6020731@my.shu.ac.uk>
2020-03-04 09:32:56 +0000

    Setup Prettier to guarantee same codeing format style

--- eHealthApp/src/pages/Patients.jsx
+++ eHealthApp/src/pages/Patients.jsx
@@ -4,21 +4,21 @@ Add description
 Author: Gergo Kekesi
 */
 
-import React from "react";
-import { IonPage, IonContent } from "@ionic/react";
-import BackButtonToolbar from "../components/BackButtonToolbar";
+import React from 'react';
+import { IonPage, IonContent } from '@ionic/react';
+import BackButtonToolbar from '../components/BackButtonToolbar';
 
 /*props:
  */
 const Patients = props => {
-	return (
-		<IonPage>
-			<BackButtonToolbar title={"Patients"} />
-			<IonContent className="ion-padding">
-				<h1>Health professional's view of patients and their data.</h1>
-			</IonContent>
-		</IonPage>
-	);
+  return (
+    <IonPage>
+      <BackButtonToolbar title={'Patients'} />
+      <IonContent className="ion-padding">
+        <h1>Health professional's view of patients and their data.</h1>
+      </IonContent>
+    </IonPage>
+  );
 };
 
 export default Patients;
--- eHealthApp/src/tests/Devices.test.jsx
+++ eHealthApp/src/tests/Devices.test.jsx
@@ -8,75 +8,79 @@ import Adapter from 'enzyme-adapter-react-16';
 import Devices from '../pages/Devices';
 import DeviceCard from '../components/DeviceCard';
 
-Enzyme.configure({adapter: new Adapter()});
+Enzyme.configure({ adapter: new Adapter() });
 
 let container = null;
 beforeEach(() => {
-    // Setup a DOM element as a render target
-    container = document.createElement('div');
-    document.body.appendChild(container);
+  // Setup a DOM element as a render target
+  container = document.createElement('div');
+  document.body.appendChild(container);
 });
 
 afterEach(() => {
-    // Cleanup on existing
-    unmountComponentAtNode(container);
-    container.remove();
-    container = null;
-})
+  // Cleanup on existing
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
 
-// #0 
-describe("Devices component", () => {
-    test("renders", () => {
-        const wrapper = shallow(<Devices />);
+// #0
+describe('Devices component', () => {
+  test('renders', () => {
+    const wrapper = shallow(<Devices />);
 
-        expect(wrapper.exists()).toBe(true);
-    });
+    expect(wrapper.exists()).toBe(true);
+  });
 });
 
 // #1 Testing if the Component renders without crashing
-test("Component renders without crashing", () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Devices />, div);
+test('Component renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Devices />, div);
 });
 
-// #2 Testing if the page's "Header" title is "Devices" 
+// #2 Testing if the page's "Header" title is "Devices"
 test("Page should have a header's title of Devices", async () => {
-    const { findByText } = render(<Devices />);
-    await findByText("Devices");
+  const { findByText } = render(<Devices />);
+  await findByText('Devices');
 });
 
 // #3 If there is no paired/connected devices found. Page should display an information
-test("Page should display a message if there is no paired/connected devices found", async () => {
-    const { findByText } = render(<Devices />);
-    await findByText("No devices found");
+test('Page should display a message if there is no paired/connected devices found', async () => {
+  const { findByText } = render(<Devices />);
+  await findByText('No devices found');
 });
 
 // #4 If there are paired/connected devices found, they should be displayed
-it("Renders paired/connected devices", async () => {
-    const bluetoothDevice1 = {
-        id: "sensor1",
-        name: "fitbit",
-        connected: "false",
-        connectionStatus: "NOT_CONNECTED",
-        onClick: () => {}
-    };
+it('Renders paired/connected devices', async () => {
+  const bluetoothDevice1 = {
+    id: 'sensor1',
+    name: 'fitbit',
+    connected: 'false',
+    connectionStatus: 'NOT_CONNECTED',
+    onClick: () => {}
+  };
 
-    jest.spyOn(global, "fetch").mockImplementation(() => 
-        Promise.resolve({
-            json: () => Promise.resolve(bluetoothDevice1)
-        })
+  jest.spyOn(global, 'fetch').mockImplementation(() =>
+    Promise.resolve({
+      json: () => Promise.resolve(bluetoothDevice1)
+    })
+  );
+
+  // Use the asynchronous version of 'act' to apply resolver promises
+  await act(async () => {
+    render(
+      <DeviceCard
+        title={bluetoothDevice1.name}
+        connected={bluetoothDevice1.connected}
+        connectionStatus={bluetoothDevice1.connectionStatus}
+      />,
+      container
     );
+  });
 
-    // Use the asynchronous version of 'act' to apply resolver promises
-    await act(async () => {
-        render(<DeviceCard 
-                    title={bluetoothDevice1.name}
-                    connected={bluetoothDevice1.connected} 
-                    connectionStatus={bluetoothDevice1.connectionStatus}/>, container)
-    });
-
-    // const comp1 = container.querySelector('p');
-    // const comp2 = container.querySelector("p");
-    // const comp3 = container.querySelector("IonCard");
-    // expect(comp1.textContent).toBe(bluetoothDevice1.title);
-})
+  // const comp1 = container.querySelector('p');
+  // const comp2 = container.querySelector("p");
+  // const comp3 = container.querySelector("IonCard");
+  // expect(comp1.textContent).toBe(bluetoothDevice1.title);
+});
