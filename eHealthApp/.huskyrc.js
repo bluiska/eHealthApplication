0972022dca86337051c2932e0ce261d6686667a8
Irek <b6020731@shu.ac.uk>
2020-03-04 12:12:35 +0000

    Fixed issues with test script

--- eHealthApp/.huskyrc.js
+++ eHealthApp/.huskyrc.js
@@ -2,6 +2,6 @@ const tasks = arr => arr.join(' && ');
 
 module.exports = {
   hooks: {
-    'pre-commit': tasks(['npm run format'])
+    'pre-commit': tasks(['npm run format', 'npm run test'])
   }
 };
