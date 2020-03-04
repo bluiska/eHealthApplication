1f7332c6a7f987583a30abc2a1505e66366069f9
Irek <b6020731@my.shu.ac.uk>
2020-03-04 11:48:11 +0000

    In progress

--- eHealthApp/src/pages/Devices.jsx
+++ eHealthApp/src/pages/Devices.jsx
@@ -7,24 +7,23 @@ Author: Ireneusz Janusz
 */
 
 // External dependencies
-import React, { useState, useEffect } from 'react'
-import { IonContent, IonPage, IonLabel, IonItem } from '@ionic/react'
-import BackButtonToolbar from '../components/BackButtonToolbar'
-import DeviceCard from '../components/DeviceCard'
+import React, { useState, useEffect } from 'react';
+import { IonContent, IonPage, IonLabel, IonItem } from '@ionic/react';
+import BackButtonToolbar from '../components/BackButtonToolbar';
+import DeviceCard from '../components/DeviceCard';
 
 // Internal dependencies
-import BluetoothSynchronisationManager from '../bluetooth/managers/BluetoothSynchronisationManager'
+import BluetoothSynchronisationManager from '../bluetooth/managers/BluetoothSynchronisationManager';
 
 // Styling
 const styles = {
-  labelContainer: { height: '100%', display: 'flex'
-  },
+  labelContainer: { height: '100%', display: 'flex' },
   noDevicesLabel: {
     textAlign: 'center',
     justifyContent: 'center',
     fontSize: '30px'
   }
-}
+};
 
 const Devices = () => {
   // Creating a state to keep a log of paired devices
@@ -38,7 +37,7 @@ const Devices = () => {
     const synchronisationListener = setInterval(() => {
       // console.log("it lives");
     }, 5000);
-  }, [getPairedDevices]);
+  }, []);
 
   /**
    * Gets the list of available Bluetooth devices using the Bluetooth Synchronisation Manager
