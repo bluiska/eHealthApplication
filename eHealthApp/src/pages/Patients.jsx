304732d6018f4c468283487a78d6e02c8255db86
Gergo <bluiska@gmail.com>
2020-03-11 16:22:36 +0000

    doctor patient selection
    asd

--- eHealthApp/src/pages/Patients.jsx
+++ eHealthApp/src/pages/Patients.jsx
@@ -4,112 +4,71 @@ Add description
 Author: Daniel Madu
 */
 
-import React, { useState, Fragment } from "react";
-import {
-  IonPage,
-  IonContent,
-  IonCard,
-  IonCardContent,
-  IonCardHeader,
-  IonCardTitle,
-  IonSelect,
-  IonSelectOption,
-  IonLabel,
-  useIonViewWillEnter,
-  IonToolbar
-} from "@ionic/react";
+import React, {Fragment } from "react";
+import { IonPage, IonContent, IonCard, IonCardContent, IonCardHeader, IonCardTitle } from "@ionic/react";
 import BackButtonToolbar from "../components/BackButtonToolbar";
+
+var dummyData = [
+	{
+		name: "Dr. Daniel",
+		ID: "Doctor-1234",
+		patients: [
+			{ id: "Patient-1234", name: "Daniel" },
+			{ id: "Patient-1234", name: "Irek" },
+			{ id: "Patient-1234", name: "Andy" },
+			{ id: "Patient-1234", name: "Greg" },
+			{ id: "Patient-1234", name: "Daniel2" },
+			{ id: "Patient-1234", name: "Irek2" },
+			{ id: "Patient-1234", name: "Andy2" },
+			{ id: "Patient-1234", name: "Greg2" }
+		]
+	},
+	{
+		name: "Dr. Irek",
+		ID: "Doctor-5678",
+		patients: [
+			{ id: "Patient-5678", name: "Sergio" },
+			{ id: "Patient-5678", name: "Chris" },
+			{ id: "Patient-5678", name: "Margory" },
+			{ id: "Patient-5678", name: "Basu" }
+		]
+	}
+];
 
 /*props:
  */
 const Patients = props => {
-  const [doctor, setDoctor] = useState("");
-  const [dbData, setDBData] = useState([]);
+	const doctor = props.match.params.docid;
 
-  useIonViewWillEnter(() => {
-    setDBData([
-      {
-        name: "Doctor Daniel",
-        ID: "Doctor-1234",
-        patients: [
-          { id: "Patient-1234", name: "Daniel" },
-          { id: "Patient-1234", name: "Irek" },
-          { id: "Patient-1234", name: "Andy" },
-          { id: "Patient-1234", name: "Greg" },
-          { id: "Patient-1234", name: "Daniel2" },
-          { id: "Patient-1234", name: "Irek2" },
-          { id: "Patient-1234", name: "Andy2" },
-          { id: "Patient-1234", name: "Greg2" }
-        ]
-      },
+	const Patient = data => {
+		return (
+			<IonCard routerDirection="forward" routerLink={`/patientoverview/doctor/${doctor}/patient/${data.patient.id}`}>
+				<IonCardHeader>
+					<IonCardTitle>Patient's Name: {data.patient.name}</IonCardTitle>
+				</IonCardHeader>
+				<IonCardContent>
+					<p>Patient Id: {data.patient.id}</p>
+				</IonCardContent>
+			</IonCard>
+		);
+	};
 
-      {
-        name: "Doctor Mark",
-        ID: "Doctor-5678",
-        patients: [
-          { id: "Patient-5678", name: "Sergio" },
-          { id: "Patient-5678", name: "Chris" },
-          { id: "Patient-5678", name: "Margory" },
-          { id: "Patient-5678", name: "Basu" }
-        ]
-      }
-    ]);
-  });
-
-  const selectDoctorHandler = event => {
-    setDoctor(event.target.value);
-  };
-
-  const Patient = data => {
-    return (
-      <IonCard
-        routerDirection="forward"
-        routerLink={
-          "/patientoverview/" + data.patient.name + "/" + data.patient.id
-        }
-      >
-        <IonCardHeader>
-          <IonCardTitle>Patient's Name: {data.patient.name}</IonCardTitle>
-        </IonCardHeader>
-        <IonCardContent>
-          <p>Patient Id: {data.patient.id}</p>
-        </IonCardContent>
-      </IonCard>
-    );
-  };
-
-  return (
-    <IonPage>
-      <IonToolbar className="ion-padding">
-        <IonLabel>Select doctor</IonLabel>
-        <IonSelect
-          interface="popover"
-          onIonChange={event => selectDoctorHandler(event)}
-        >
-          {dbData.map((doctor, index) => (
-            <IonSelectOption value={doctor.ID} key={index}>
-              {doctor.name}
-            </IonSelectOption>
-          ))}
-        </IonSelect>
-      </IonToolbar>
-      <BackButtonToolbar title={"Patients"} />
-      <IonContent className="ion-padding">
-        {doctor && (
-          <Fragment>
-            <h2>Assigned Patients</h2>
-            {dbData
-              .filter(doc => doc.ID === doctor)
-              .map((patients, i1) =>
-                patients.patients.map((patient, i2) => (
-                  <Patient key={i1 + i2} patient={patient} />
-                ))
-              )}
-          </Fragment>
-        )}
-      </IonContent>
-    </IonPage>
-  );
+	return (
+		<IonPage>
+			{console.log("Rendered!")}
+			<BackButtonToolbar title={"Patients"} />
+			<IonContent className="ion-padding">
+				{doctor && (
+					<Fragment>
+						<h2>Assigned Patients</h2>
+						{dummyData
+							.filter(doc => doc.name === doctor)
+							.map((patients, i1) => patients.patients.map((patient, i2) => <Patient key={i1 + i2} patient={patient} />))}
+					</Fragment>
+				)}
+			</IonContent>
+		</IonPage>
+	);
 };
 
 export default Patients;
