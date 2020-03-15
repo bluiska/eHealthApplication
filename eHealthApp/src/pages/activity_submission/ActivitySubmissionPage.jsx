a754bafc39e1b5a699036042614f135706062670
Gergo <bluiska@gmail.com>
2020-03-15 17:00:14 +0000

    Added submission for exercises too
    Need validation still

--- eHealthApp/src/pages/activity_submission/ActivitySubmissionPage.jsx
+++ eHealthApp/src/pages/activity_submission/ActivitySubmissionPage.jsx
@@ -6,78 +6,78 @@ import ActivityQueries from "./../../queries/ActivityQueries";
 import { withRouter } from "react-router-dom";
 
 const ActivitySubmissionPage = props => {
-  const [showSubmitAlert, setShowSubmitAlert] = useState(false);
-  const [submitAlertContent, setSubmitAlertContent] = useState(false);
-  const HEADER = {
-    SUCCESS: "Submission successful.",
-    FAIL: "Submission failed!"
-  };
-  return (
-    <IonPage>
-      <IonAlert
-        isOpen={showSubmitAlert}
-        onDidDismiss={() => {
-          setShowSubmitAlert(false);
-          submitAlertContent.header === HEADER.SUCCESS &&
-            props.history.goBack();
-        }}
-        header={submitAlertContent.header}
-        message={submitAlertContent.message}
-        buttons={["OK"]}
-      />
-      <BackButtonToolbar title={props.title} />
-      <IonContent className="ion-padding">{props.children}</IonContent>
-      <FooterSubmitButton
-        onSubmit={() => {
-          if (props.submissionType === "measurement") {
-            ActivityQueries.uploadNewMeasurement(props.patientId, {
-              type: props.measurementType,
-              data: { ...props.submitData }
-            })
-              .then(res => {
-                if (res && res.ID && res.ID.length > 0) {
-                  setSubmitAlertContent({
-                    header: HEADER.SUCCESS,
-                    message: props.successMessage
-                  });
-                } else {
-                  setSubmitAlertContent({
-                    header: HEADER.FAIL,
-                    message: props.failMessage
-                  });
-                }
-                setShowSubmitAlert(true);
-              })
-              .catch(() => {
-                setSubmitAlertContent({
-                  header: HEADER.FAIL,
-                  message: props.failMessage
-                });
-                setShowSubmitAlert(true);
-              });
-          } else {
-            ActivityQueries.uploadNewExercise(props.patientId, {
-              type: props.measurementType,
-              ...props.submitData
-            }).then(res => {
-              if (res && res.ID && res.ID.length > 0) {
-                setSubmitAlertContent({
-                  header: "Submission successful.",
-                  message: props.successMessage
-                });
-              } else {
-                setSubmitAlertContent({
-                  header: "Submission failed!",
-                  message: props.failMessage
-                });
-              }
-              setShowSubmitAlert(true);
-            });
-          }
-        }}
-      />
-    </IonPage>
-  );
+	const [showSubmitAlert, setShowSubmitAlert] = useState(false);
+	const [submitAlertContent, setSubmitAlertContent] = useState(false);
+	const HEADER = {
+		SUCCESS: "Submission successful.",
+		FAIL: "Submission failed!"
+	};
+	return (
+		<IonPage>
+			<IonAlert
+				isOpen={showSubmitAlert}
+				onDidDismiss={() => {
+					setShowSubmitAlert(false);
+					submitAlertContent.header === HEADER.SUCCESS && props.history.goBack();
+				}}
+				header={submitAlertContent.header}
+				message={submitAlertContent.message}
+				buttons={["OK"]}
+			/>
+			<BackButtonToolbar title={props.title} />
+			<IonContent className="ion-padding">{props.children}</IonContent>
+			<FooterSubmitButton
+				onSubmit={() => {
+					console.log(props.submitData);
+					if (props.submissionType === "measurement") {
+						ActivityQueries.uploadNewMeasurement(props.patientId, {
+							type: props.measurementType,
+							data: { ...props.submitData }
+						})
+							.then(res => {
+								if (res && res.ID && res.ID.length > 0) {
+									setSubmitAlertContent({
+										header: HEADER.SUCCESS,
+										message: props.successMessage
+									});
+								} else {
+									setSubmitAlertContent({
+										header: HEADER.FAIL,
+										message: props.failMessage
+									});
+								}
+								setShowSubmitAlert(true);
+							})
+							.catch(() => {
+								setSubmitAlertContent({
+									header: HEADER.FAIL,
+									message: props.failMessage
+								});
+								setShowSubmitAlert(true);
+							});
+					} else {
+						ActivityQueries.uploadNewExercise(props.patientId, {
+							type: props.measurementType,
+							...props.submitData
+						}).then(res => {
+							if (res && res.ID && res.ID.length > 0) {
+								setSubmitAlertContent({
+									header: "Submission successful.",
+									message: props.successMessage
+								});
+							} else {
+								setSubmitAlertContent({
+									header: "Submission failed!",
+									message: props.failMessage
+								});
+							}
+							setShowSubmitAlert(true);
+						});
+					}
+				}}
+			/>
+		</IonPage>
+	);
 };
 
 export default withRouter(ActivitySubmissionPage);
--- eHealthApp/src/queries/ActivityQueries.js
+++ eHealthApp/src/queries/ActivityQueries.js
@@ -3,49 +3,47 @@ import BackendAccess from "../utilities/BackendAccess";
 var ActivityQueries = {};
 
 ActivityQueries.getActivitiesByDateRange = (patientID, from, to) => {
-  return BackendAccess.IssueODataRequest({
-    requestType: "GET",
-    entityType: "Activities",
-    query: {
-      $filter: `patient/ID eq '${patientID}' and date(timestamp) ge ${from} and date(timestamp) le ${to} patient/ID eq '${patientID}'`
-    }
-  });
+	return BackendAccess.IssueODataRequest({
+		requestType: "GET",
+		entityType: "Activities",
+		query: {
+			$filter: `patient/ID eq '${patientID}' and date(timestamp) ge ${from} and date(timestamp) le ${to} patient/ID eq '${patientID}'`
+		}
+	});
 };
 
 ActivityQueries.getTodaysActivities = (patientID, today) => {
-  return BackendAccess.IssueODataRequest({
-    requestType: "GET",
-    entityType: "Activities",
-    query: {
-      $filter: `patient/ID eq '${patientID}' and date(timestamp) eq ${today}`
-    }
-  });
+	return BackendAccess.IssueODataRequest({
+		requestType: "GET",
+		entityType: "Activities",
+		query: {
+			$filter: `patient/ID eq '${patientID}' and date(timestamp) eq ${today}`
+		}
+	});
 };
 
 ActivityQueries.uploadNewExercise = (patient, exercise) => {
-  return BackendAccess.IssueODataRequest({
-    requestType: "POST",
-    entityType: exercise.type,
-    entityBody: {
-      name: "exercise",
-      patient: { ID: patient },
-      timestamp: new Date(),
-      ...exercise
-    }
-  });
+	return BackendAccess.IssueODataRequest({
+		requestType: "POST",
+		entityType: exercise.type,
+		entityBody: {
+			patient: { ID: patient },
+			timestamp: new Date(),
+			...exercise
+		}
+	});
 };
 
 ActivityQueries.uploadNewMeasurement = async (patient, measurement) => {
-  return BackendAccess.IssueODataRequest({
-    requestType: "POST",
-    entityType: measurement.type,
-    entityBody: {
-      name: "measurement",
-      patient: { ID: patient },
-      timestamp: new Date(),
-      ...measurement.data
-    }
-  });
+	return BackendAccess.IssueODataRequest({
+		requestType: "POST",
+		entityType: measurement.type,
+		entityBody: {
+			patient: { ID: patient },
+			timestamp: new Date(),
+			...measurement.data
+		}
+	});
 };
 
 export default ActivityQueries;
